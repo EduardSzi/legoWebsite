a65f0aa6880556ce3e2b303ca05d98722bcc35fb
eduardSzi <szed1012@hs-karlsruhe.de>
2023-10-10 21:41:58 +0200

    Added colors and materials loader, optimized the camera and added some buttons to config the viewer

--- views/main.js
+++ views/main.js
@@ -2,50 +2,61 @@ import * as THREE from 'three';
 import { LDrawLoader } from 'three/addons/loaders/LDrawLoader.js';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-const loader = new LDrawLoader();
-
-renderer.setSize(734, 296);
-
-// Get container for viewer window
-const container = document.getElementById('webgl-container');
-
-
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild(renderer.domElement); // Add renderer to container
-camera.position.z = 5;
+// Set working variables
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+const loader = new LDrawLoader();
 
-// Pfad zu den Teilen
+// Set camera position
+camera.position.z = 400;
+
+// Set background color
+renderer.setClearColor(0x8f8b8a);
+// Activate shadows
+renderer.shadowMap.enabled = true;
+
+// Set window size for viewer window
+renderer.setSize(824, 469);
+
+// Get container for viewer window
+const container = document.getElementById('webgl-container');
+
+// Add renderer to container
+container.appendChild(renderer.domElement); 
+
+// Path to the parts
 var partsLibraryPath = "/models/ldraw/officialLibrary/";
 loader.setPartsLibraryPath(partsLibraryPath);
 
-// Lade eine LDraw-Ressource
+// Load materials
+await loader.preloadMaterials( 'models/ldraw/officialLibrary/LDCfgalt.ldr' );
+
+// Load LDraw-Ressource
 loader.load(
-  // Ressourcen-URL
+  // Ressource URL
   'models/BUILD_Schere-Stein-Papier_v1.0.ldr',
-  // Aufgerufen, wenn die Ressource geladen ist
+  // Called, when ressource is loaded
   function (group) {
-    // Füge die Gruppe zur Szene hinzu
+    // Add group to scene
     scene.add(group);
   },
-  // Aufgerufen während des Ladevorgangs
+  // Called while loading process
   function (xhr) {
     console.log((xhr.loaded / xhr.total * 100) + '% geladen');
   },
-  // Aufgerufen, wenn beim Laden Fehler auftreten
+  // Called, when error appears
   function (error) {
     console.log('Ein Fehler ist aufgetreten');
   }
 );
 
-// Fehlerbehandlung
+// Error handling
 if (WebGL.isWebGLAvailable()) {
   // Initiiere Funktion oder andere Initialisierungen hier
   animate();
@@ -53,3 +64,38 @@ if (WebGL.isWebGLAvailable()) {
   const warning = WebGL.getWebGLErrorMessage();
   container.appendChild(warning);
 }
+
+$(document).ready( function() {
+
+
+  $("#resetBtn").click(function(){
+    $("#fileform").transition('show');
+    $("#progress").transition('show');
+    $('#buttonBar').transition('hide');
+    $("#canvas").transition('hide');
+    $("#webgl").api("remove loading");
+    return false;
+  });
+
+  $("#bkgColorBtn").click(function(){
+    $('#model-bg-color').click();
+  });
+
+  $("#physicalBtn").click(function(){
+    $("#webgl").api("set loading");
+    if (scene.loader.physicalRenderingAge > 0) {
+      scene.setPhysicalRenderingAge(0);
+    }
+    else {
+      scene.setPhysicalRenderingAge(20);
+    }
+  });
+
+  $('#model-bg-color').on('change', function (e) {
+      LDR.Options.bgColor = parseInt(e.target.value.replace(/^#/, ''), 16);
+      $("#bkgColorBtn").css('background-color', e.target.value);
+      $("#canvas").css('background', e.target.value);
+      scene.scene.background = new THREE.Color(LDR.Options.bgColor);
+      scene.render();
+  });
+});
